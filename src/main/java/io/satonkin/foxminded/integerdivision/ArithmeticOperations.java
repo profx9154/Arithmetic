daecfffb91084932e385b80be9b1f5debff62097
SONY VAIO <profx9154@gmail,com>
2023-12-20 20:58:07 +0300

    create a few classes

--- src/main/java/io/satonkin/foxminded/integerdivision/ArithmeticOperations.java
+++ src/main/java/io/satonkin/foxminded/integerdivision/ArithmeticOperations.java
@@ -1,4 +1,15 @@
 package io.satonkin.foxminded.integerdivision;
 
 public class ArithmeticOperations {
+    public int division(int dividend, int divisor) {
+
+        int quotient = 0;
+        try {
+            quotient = dividend / divisor;
+
+        } catch (ArithmeticException DivideByZero) {
+            System.out.println("You can't divide by zero");
+        }
+        return quotient;
+    }
 }
---
+++ src/main/java/io/satonkin/foxminded/integerdivision/Main.java
@@ -0,0 +1,11 @@
+package io.satonkin.foxminded.integerdivision;
+
+public class Main {
+    public static void main(String[] args) {
+        ArithmeticOperations arithmeticOperations = new ArithmeticOperations();
+
+        System.out.println(arithmeticOperations.division(5,1));
+    }
+
+
+}
