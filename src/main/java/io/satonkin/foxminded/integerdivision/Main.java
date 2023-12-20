b2c15119cf948751d01ed7bc9b24be71814a53e4
SONY VAIO <profx9154@gmail,com>
2023-12-20 21:10:17 +0300

    Create Test

--- src/main/java/io/satonkin/foxminded/integerdivision/Main.java
+++ src/main/java/io/satonkin/foxminded/integerdivision/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ArithmeticOperations arithmeticOperations = new ArithmeticOperations();
 
-        System.out.println(arithmeticOperations.division(5,1));
+        System.out.println(arithmeticOperations.division(5, 1));
     }
 
 
--- src/test/java/io/satonkin/foxminded/integerdivision/ArithmeticOperationsTest.java
+++ src/test/java/io/satonkin/foxminded/integerdivision/ArithmeticOperationsTest.java
@@ -8,21 +8,22 @@ class ArithmeticOperationsTest {
     private short ArithmeticException;
 
     @Test
-    void integerDivision(){
+    void integerDivision() {
         ArithmeticOperations arithmeticOperations = new ArithmeticOperations();
-        assertEquals(10, arithmeticOperations.division(20,2));
+        assertEquals(10, arithmeticOperations.division(20, 2));
     }
+
     @Test
-    void divisionByANegativeNumber(){
+    void divisionByZero() throws ArithmeticException {
         ArithmeticOperations arithmeticOperations = new ArithmeticOperations();
-        assertEquals(-25, arithmeticOperations.division(150,-6));
+        assertEquals(ArithmeticException, arithmeticOperations.division(50, 0));
 
     }
 
     @Test
-    void divisionByZero() throws ArithmeticException{
+    void divisionByANegativeNumber() {
         ArithmeticOperations arithmeticOperations = new ArithmeticOperations();
-        assertEquals(ArithmeticException, arithmeticOperations.division(50,0));
+        assertEquals(-25, arithmeticOperations.division(150, -6));
 
     }
 
