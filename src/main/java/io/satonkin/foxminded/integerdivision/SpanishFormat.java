6e6e98d9463fb51958c92e408069658bf82385be
SONY VAIO <profx9154@gmail,com>
2024-03-06 12:06:27 +0300

    Change Formater

--- src/main/java/io/satonkin/foxminded/integerdivision/SpanishFormat.java
+++ src/main/java/io/satonkin/foxminded/integerdivision/SpanishFormat.java
@@ -1,69 +1,59 @@
 package io.satonkin.foxminded.integerdivision;
 
 public class SpanishFormat implements Formatter {
+  private static final String LINE_SEPARATOR = "\n";
+
   @Override
   public String format(Result result) {
     StringBuilder output = new StringBuilder();
-    output.append(createFirstLine(result));
+    output.append(createFirstСolumn(result));
     output.append(createSecondLine(result));
     System.out.println(output);
-
     return "";
   }
 
-  public StringBuilder createFirstLine(Result result){
-    StringBuilder firstLine=new StringBuilder();
-    firstLine.append(addDashToNumber(result.getDividend()));
-    firstLine.append(addVerticalLine());
-    firstLine.append(result.getDivisor()+"\n");
-    firstLine.append(" "+result.getSubtrahend().get(0));
-    firstLine.append(addSomeCharInString(calculateLenghtDigit(result.getDividend())-1,' '));
-    firstLine.append(addVerticalLine());
-    firstLine.append(addSomeCharInString(calculateLenghtDigit(result.getQuotient()), '_'));
-    firstLine.append("\n");
-    firstLine.append(addSomeCharInString(1,' '));
-    firstLine.append(addSomeCharInString(1,'_'));
-    firstLine.append(addSomeCharInString(calculateLenghtDigit(result.getDividend())-1,' '));
-    firstLine.append(addVerticalLine());
-    firstLine.append(result.getQuotient());
-    firstLine.append("\n");
-    return firstLine;
+  public StringBuilder createFirstСolumn(Result result) {
+    StringBuilder firstColumn = new StringBuilder();
+    String firstLine = addDashToMinuend(result.getDividend()) + "|" + result.getDivisor() + LINE_SEPARATOR;
+    String secondLine = " " + result.getSubtrahend().get(0) + addCharInString(calculateLenghtDigit(result.getDividend()) - 1, ' ') +
+      "|" + addCharInString(calculateLenghtDigit(result.getQuotient()), '_') + LINE_SEPARATOR;
+    String thirdLine = " " + addCharInString(1, '_') +
+      addCharInString(calculateLenghtDigit(result.getDividend()) - 1, ' ') + "|" + result.getQuotient() + LINE_SEPARATOR;
+    firstColumn.append(firstLine + secondLine + thirdLine);
+    return firstColumn;
   }
 
-  public StringBuilder createSecondLine(Result result){
-    StringBuilder secondLine=new StringBuilder();
-    secondLine.append(addDashToNumber(result.getMinuend().get(1)));
+  public StringBuilder createSecondLine(Result result) {
+    StringBuilder secondLine = new StringBuilder();
+
 
     return secondLine;
   }
 
-  public StringBuilder createThirdLine(Result result){
-    StringBuilder secondLine=new StringBuilder();
-    secondLine.append(addDashToNumber(result.getMinuend().get(1)));
+  public StringBuilder createThirdLine(Result result) {
+    StringBuilder secondLine = new StringBuilder();
+    secondLine.append(addDashToMinuend(result.getMinuend().get(1)));
 
     return secondLine;
   }
 
-  public String addDashToNumber(int number){
-    String ouputString="_"+number;
+  public String addDashToMinuend(int number) {
+    String ouputString = "_" + number;
     return ouputString;
   }
-  public String addVerticalLine(){
-    return "|";
-  }
 
-  public String addSomeCharInString(int numberOfSymbols, char symbol) {
 
+  public String addCharInString(int numberOfSymbols, char symbol) {
     StringBuilder string = new StringBuilder();
     for (int i = 0; i < numberOfSymbols; i++) {
       string.append(symbol);
     }
     return string.toString();
   }
+
   private int calculateLenghtDigit(int i) {
     return (int) Math.log10(i) + 1;
   }
-
 
 
 }
