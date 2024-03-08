d5ab3d57cad24bdc0028cda9b5bb212eeb081470
SONY VAIO <profx9154@gmail,com>
2024-03-08 15:31:38 +0300

    Finish Calculate Test

--- src/test/java/io/satonkin/foxminded/integerdivision/CalculatorTest.java
+++ src/test/java/io/satonkin/foxminded/integerdivision/CalculatorTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 class CalculatorTest {
+  Calculator calculator = new Calculator();
 
   @Test
   void testDivideExpectationQuotient() {
-    Calculator calculator = new Calculator();
-    Result result = calculator.divide(78945,4);
-    int expectationQuotient= result.getQuotient();
+    Result result = calculator.divide(78945, 4);
+    int expectationQuotient = result.getQuotient();
     Assertions.assertEquals(19736, expectationQuotient);
 
   }
@@ -19,9 +19,8 @@ class CalculatorTest {
 
   @Test
   void testDivideExpectationReminder() {
-    Calculator calculator = new Calculator();
-    Result result = calculator.divide(78945,4);
-    int expectationReminder= result.getReminder();
+    Result result = calculator.divide(78945, 4);
+    int expectationReminder = result.getReminder();
     Assertions.assertEquals(1, expectationReminder);
 
   }
@@ -29,10 +28,9 @@ class CalculatorTest {
 
   @Test
   void testDivideExpectationListSubtrahend() {
-    Calculator calculator = new Calculator();
-    Result result = calculator.divide(78945,4);
-    ArrayList<Integer> expectationListSubtrahend=result.getSubtrahend();
-    ArrayList<Integer>realListSubtrahend=new ArrayList<Integer>();
+    Result result = calculator.divide(78945, 4);
+    ArrayList<Integer> expectationListSubtrahend = result.getSubtrahend();
+    ArrayList<Integer> realListSubtrahend = new ArrayList<Integer>();
     realListSubtrahend.add(4);
     realListSubtrahend.add(36);
     realListSubtrahend.add(28);
@@ -44,10 +42,9 @@ class CalculatorTest {
 
   @Test
   void testDivideExpectationListMinuend() {
-    Calculator calculator = new Calculator();
-    Result result = calculator.divide(78945,4);
-    ArrayList<Integer> expectationListMinuend=result.getMinuend();
-    ArrayList<Integer>realListMinuend=new ArrayList<Integer>();
+    Result result = calculator.divide(78945, 4);
+    ArrayList<Integer> expectationListMinuend = result.getMinuend();
+    ArrayList<Integer> realListMinuend = new ArrayList<Integer>();
     realListMinuend.add(7);
     realListMinuend.add(38);
     realListMinuend.add(29);
@@ -55,25 +52,60 @@ class CalculatorTest {
     realListMinuend.add(25);
     Assertions.assertEquals(expectationListMinuend, realListMinuend);
   }
+
   @Test
   void testBreakDownIntoIndividualNumbers() {
-    ArrayList<Integer>testRealStringDividend=new ArrayList<>();
+    ArrayList<Integer> testRealStringDividend = new ArrayList<>();
     testRealStringDividend.add(7);
     testRealStringDividend.add(8);
     testRealStringDividend.add(9);
     testRealStringDividend.add(4);
     testRealStringDividend.add(5);
-    int testDividend=78945;
+    int testDividend = 78945;
     String[] testStringDividend = String.valueOf(testDividend).split("");
     ArrayList<Integer> testExpectationDividend = new ArrayList<>();
     for (int i = 0; i < testStringDividend.length; i++) {
       testExpectationDividend.add(Integer.parseInt(testStringDividend[i].toString()));
     }
-    Assertions.assertEquals(testRealStringDividend,testExpectationDividend);
+    Assertions.assertEquals(testRealStringDividend, testExpectationDividend);
 
   }
 
   @Test
-  void checkByExceptionByZero() {
+  void testCheckByExceptionByZero() {
+    Exception exception = Assertions.assertThrows(IllegalArgumentException.class,
+      () -> {
+        calculator.divide(78945,0);
+      });
+
+    String expectedMessage = "Divisor can't be zero";
+    String actualMessage = exception.getMessage();
+
+    Assertions.assertEquals(actualMessage, expectedMessage);
+  }
+
+  @Test
+  void testCheckByExceptionNumberShouldBePositive() {
+    Exception exception = Assertions.assertThrows(ArithmeticException.class,
+      () -> {
+        calculator.divide(-78945,5);
+      });
+
+    String expectedMessage = "Dividend and divisor must be positive";
+    String actualMessage = exception.getMessage();
+
+    Assertions.assertEquals(actualMessage, expectedMessage);
+  }
+
+  @Test
+  void testCheckByExceptionDivisorMoreDividend() {
+    Exception exception = Assertions.assertThrows(ArithmeticException.class,
+      () -> {
+        calculator.divide(1,100);
+      });
+    String expectedMessage = "Divisor more Dividend answer 0.01" ;
+    String actualMessage = exception.getMessage();
+
+    Assertions.assertEquals(actualMessage, expectedMessage);
   }
 }
